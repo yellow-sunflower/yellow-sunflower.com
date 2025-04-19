67f89a1cbdf270c1bd832b2ad1ddf2c12bbe4dee
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-04-19 13:37:22 +0200

    footer

--- script/hero.js
+++ script/hero.js
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   createWordChanger("changing-word-hero", [
     "commit",
-    "package",
+    "project",
     "discussion",
     "graphic",
     "story",
